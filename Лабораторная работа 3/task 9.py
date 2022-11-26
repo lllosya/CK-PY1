549708555ca7e7ea17713281209496f9e74745d2
lllosya <119103857+lllosya@users.noreply.github.com>
2022-11-26 23:39:48 +0300

    Update task 9.py

--- Лабораторная работа 3/task 9.py
+++ Лабораторная работа 3/task 9.py
@@ -5,11 +5,8 @@ increase = 0.03  # рост цен
 
 money_capital = 0  # количество денег, чтобы прожить 10 месяцев
 
-while True:
+for months in range(months):
     delta = spend - salary
-    if months == 0:
-        break
-
     months -= 1
     money_capital += delta
     spend *= 1 + increase
